13be558262e45ac1eedc099320c028aff5c4322d
Dannel Alon <alon@Dannels-MacBook-Pro.local>
2020-01-02 09:59:00 -0500

    Adding flow conditions

--- src/com/myapp/Lesson2.kt
+++ src/com/myapp/Lesson2.kt
@@ -1,5 +1,24 @@
 package com.myapp
 
 fun main(args : Array<String>) {
-    // 1. If expression
+    // 1. If expression - "if" condition returns a value in Kotlin
+    var isSomething : Boolean = true
+    if (isSomething) {
+        println("Something is true!")
+    } else {
+        println("Something is false!")
+    }
+
+    // If returning value then both if & else must be used
+    // the else branch is mandatory if using if as expression
+    var lotteryMsg : String = if (true) {
+        "YOU HAVE WON THE LOTTERY"
+    } else {
+        "You loser!"
+    }
+    println("Lottery Msg: $lotteryMsg")
+
+    // Brackets are optional
+    var isRandom : String = if (true) "Yes, it is random" else "No, it is not!"
+    println(isRandom)
 }
